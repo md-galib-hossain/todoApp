d0ecf8195401cdd51ed3827ccc8b44e7c94b02f6
md-galib-hossain <mdgalib23@gmail.com>
2023-07-09 11:44:38 +0600

    Database connection established

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from 'express';
+import connectToDatabase from './db';
 const application = express()
 const PORT = 1337
+connectToDatabase()
 application.get("/ping",(request:Request,response:Response)=>{
     response.send("Pong")
 })
