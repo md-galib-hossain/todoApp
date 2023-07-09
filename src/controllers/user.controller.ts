8007310c1a7c966ff758d2bc61d6962443585b0c
md-galib-hossain <mdgalib23@gmail.com>
2023-07-09 20:30:40 +0600

    Login and jwt token

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,29 +1,65 @@
-import { Request, Response } from "express";
-import bcrypt from "bcrypt";
-import User from "../models/user-model";
+import bcrypt from "bcrypt"
+import { Request, Response } from "express"
+import jwt from "jsonwebtoken"
+import { Types } from "mongoose"
+import User from "../models/user-model"
+import {IUser} from "../types"
 
-const createUser = async (request: Request, response: Response) => {
+const getUserToken = (_id: string | Types.ObjectId) => {
+  const authenticatedUserToken = jwt.sign({ _id }, "express", {
+    expiresIn: "7d",
+  })
+  return authenticatedUserToken
+}
+
+export const createUser = async (request: Request, response: Response) => {
   try {
     const { name, email, password } = request.body
 
-    // Finding if the user already exists
     const existingUser = await User.findOne({ email })
     if (existingUser) {
-      return response.status(409).send("User already exists")
+      return response.status(409).send("user already exist")
     }
 
-    // encrypting the password
     const hashedPassword = await bcrypt.hash(password, 12)
-// creating user
+
     const user = await User.create({
-      name,
-      email,
-      hashedPassword,
+      name: name,
+      email: email,
+      password: hashedPassword,
     })
-    return response.status(201).send({message:"User created succesfully"})
+
+    return response.status(201).send({ message: "User created successfully" })
   } catch (error) {
-    console.log("error in creating user", error)
+    console.log("error in createUser", error)
     throw error
   }
-};
-export default createUser
+}
+export const loginUser = async (request: Request, response: Response) => {
+  try {
+    const { email, password }: IUser = request.body
+    const existingUser = await User.findOne({ email })
+    if (!existingUser) {
+      return response.status(409).send({ message: "User doesn't exist" })
+    }
+    const isPasswordIdentical = await bcrypt.compare(
+      password,
+      existingUser.password
+    )
+    if (isPasswordIdentical) {
+      const token = getUserToken(existingUser._id)
+      return response.send({
+        token,
+        user: {
+          email: existingUser.email,
+          name: existingUser.name,
+        },
+      })
+    } else {
+      return response.status(400).send({ message: "Wrong credentials" })
+    }
+  } catch (error) {
+    console.log("error in loginUser", error)
+    throw error
+  }
+}
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,7 +1,8 @@
 import express from 'express';
-import createUser from '../controllers/user.controller';
+import {createUser,loginUser} from '../controllers/user.controller';
 
 const userRoutes = express.Router()
 userRoutes.route("/create").post(createUser)
+userRoutes.route("/login").post(loginUser)
 
 export default userRoutes;
